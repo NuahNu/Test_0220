ce6972ae40fde581510cf3bf735cfd7956d6567b
NuahNu <104414203+NuahNu@users.noreply.github.com>
2026-02-20 17:33:12 +0900

    test

---
+++ Assets/test.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+#region test
+/*
+
+*/
+#endregion
+
+public class test : MonoBehaviour
+{
+    #region 인스펙터
+    
+    #endregion
+
+    #region 내부 변수
+    
+    #endregion
+
+    void Awake()
+    {
+
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
