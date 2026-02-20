ee099810faa8751eb25642997f78210d9386338f
GMJune <jbumjune1224@gmail.com>
2026-02-20 17:44:44 +0900

    이동 로직 구현
    test.cs에 GetAxis로 이동 로직 구현

--- Assets/test.cs
+++ Assets/test.cs
@@ -12,16 +12,16 @@ using UnityEngine;
 public class test : MonoBehaviour
 {
     #region 인스펙터
-    
+
     #endregion
 
     #region 내부 변수
-    
+    public float speed = 5f;
     #endregion
 
     void Awake()
     {
-
+        
     }
 
     void Start()
@@ -31,6 +31,11 @@ public class test : MonoBehaviour
 
     void Update()
     {
-        
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = new Vector3(x, 0, z);
+
+        transform.Translate(move * speed * Time.deltaTime);
     }
 }
